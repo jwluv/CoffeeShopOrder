f32f1c078ede8ba74382b72def52299a4f4ca076
jwluv <jwluv1220@gmail.com>
2018-11-29 16:26:11 +0900

    Add Google map

--- app/src/main/java/com/lyna/www/coffeeshoporder/MainActivity.java
+++ app/src/main/java/com/lyna/www/coffeeshoporder/MainActivity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +29,9 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
+    private DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
     OrderDBHelper orderDBHelper;
     SQLiteDatabase mdb;
 
@@ -34,6 +44,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        initializeNavigationView();
 
         orderDBHelper = new OrderDBHelper(this, "orders.db", null, 1);
         mdb = orderDBHelper.getWritableDatabase();
@@ -63,6 +79,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         listViewOrderList.setAdapter(arrayAdapter);
         listViewOrderList.setOnItemClickListener(this);
+    }
+
+    public void initializeNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,0,0);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_navigationView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.first){
+                    getFragmentManager().beginTransaction().replace(R.id.main_framelayout, new NoticeFragment()).commit();
+
+                }
+                if(item.getItemId() == R.id.second){
+                    getFragmentManager().beginTransaction().replace(R.id.main_framelayout, new LocationFragment()).commit();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
     }
 
     @Override
